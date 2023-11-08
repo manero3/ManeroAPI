cb76d1db5f7594861b02d237181149c6d6f9c064
firecracker6 <saxe.leah@gmail.com>
2023-11-08 11:15:46 +0100

    pushing to develop firstname lastname

--- Contexts/ApplicationUser.cs
+++ Contexts/ApplicationUser.cs
@@ -6,13 +6,16 @@ namespace ManeroBackendAPI.Contexts
 {
     public class ApplicationUser : IdentityUser<Guid>  // Notice the <Guid> generic parameter
     {
-        // No need to redefine the Id property. It's already defined as Guid in the IdentityUser<Guid> base class.
+      
 
-        public string? FullName { get; set; }   // You might want to add a setter here if you want to modify it.
+        public string? FullName { get; set; }  
         public string? RefreshToken { get; set; }
         public string? OAuthProvider { get; set; }
         public string? OAuthId { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
 
         public virtual ICollection<RefreshToken> RefreshTokens { get; set; } = new HashSet<RefreshToken>();
 
